824eb5f4b306b34e8f9dc8f7dce999ccbdaac925
Luiz Guilherme <luizsilva052005@gmail.com>
2024-01-10 23:26:41 -0300

    Melhorando o codigo do ex028

--- ex028.py
+++ ex028.py
@@ -1,9 +1,13 @@
 from random import randint
 pc = randint(1,5)
-user = int(input('tente advinhar o numero de 1 a 5 que eu pensei: '))
-if user == pc:
-    print('parabéns você acetou!')
-else:
-    while user < pc or user > pc:
-        user = int(input('quase, tente mais uma vez: '))
-print('parabéns você acetou!')
+acertou = False
+
+while acertou == False:
+    user = int(input('tente advinhar o numero de 1 a 5 que eu pensei: '))
+    if user > pc:
+        print('Quase, tente um número menor...')
+    elif user < pc: 
+        print('Quase, tene um número maior...')
+    elif user == pc:
+        acertou = True
+        print('Você acertou, Parabéns!')
